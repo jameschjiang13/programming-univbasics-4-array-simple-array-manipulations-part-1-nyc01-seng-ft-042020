f2c5bf83bc29618c9306d70b19c5886dbacfec2e
James Jiang <monochrome473@gmail.com>
2020-04-10 21:50:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array, string)
+  array.push (string)
+end
+
+def using unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, number)
+  number times.do
+    array.pop
+  end
+end
